8909aba8d93c632c71c9e5c348cec0afa779731b
prince kumar <125152568+Userride@users.noreply.github.com>
2025-02-01 00:32:23 +0530

    Update SingleChat.js

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -16,7 +16,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 
-const ENDPOINT = "https://chat-backend-2-6o67.onrender.com"; // Backend URL
+const ENDPOINT = "https://chat-backend-3-xugy.onrender.com"; // Backend URL
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
